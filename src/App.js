f030a5082e6658667c0a8dbbba1cf9abeeadf474
then0403 <39440926+then0403@users.noreply.github.com>
2023-02-12 08:22:55 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
+import ImageSlider from './components/ImageSlider.jsx';
+import NavBar from './components/NavBar.jsx';
+import Offers from './components/Offers.jsx';
+import Plan from './components/Plan.jsx';
+import Rooms from './components/Rooms.jsx';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
